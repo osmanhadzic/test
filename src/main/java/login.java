e34cb89393be0afbbd5d516a322d4afb897d9b26
elviraalic21 <elvira.alic21@gmail.com>
2020-04-28 01:10:06 +0200

    Dodana funkcionalnost Button-u

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -1,4 +1,27 @@
 package main.java;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+
 public class login {
+    @FXML
+    PasswordField lozinka;
+    public void klik(ActionEvent actionEvent) {
+        if(lozinka.getLength() < 8) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Pogrešna lozinka!");
+            alert.setHeaderText("Unijeli ste pogresnu lozinku");
+            alert.setContentText("Lozinka mora imati bar 8 karaktera!");
+            alert.show();
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Uspješan login!");
+            alert.setHeaderText("Uspješna prijava");
+            alert.show();
+
+        }
+    }
 }
